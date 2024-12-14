120ccb218a41e595268dc4d2bcf3ce7bf7524333
nitin.kumarmaurya@raze.co.in <128896918+nitinkumarmaurya1@users.noreply.github.com>
2024-12-14 19:42:53 +0530

    Authentication via clerk (sign-in/sign-up)

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,5 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function DashboardPage() {
   return (
-    <p>Dashboard Page</p>
+    <div>
+      <p>Dashboard Page</p>
+      <UserButton afterSignOutUrl="/" />
+    </div>
   );
 }
